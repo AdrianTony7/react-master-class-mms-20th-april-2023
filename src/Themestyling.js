20efbe317d72940e549263aee36fba6d240691fd
JEBREED <info@jetbreed.com>
2023-07-20 10:21:24 +0100

    Implementing full begin!

--- src/Themestyling.js
+++ src/Themestyling.js
@@ -2,30 +2,47 @@ import React from "react";
 
 const person = {
     name: 'Gregorio Y. Zara',
-    theme: {
-        backgroundColor: 'black',
-        color: 'pink'
-    },
     theme1: {
+        backgroundColor: 'black',
+        color: 'pink',
+        height:'100vh'
+    },
+    theme2: {
         height:'100px', 
-        width: '100px'
-    }
+        width: '100px',
+        borderRadius:'50px'
+    },
+    imgStyle:{
+      display:'flex',
+        justifyContent:'center'
+    },
+    listStyle:{
+      textAlign:"center",
+      listStyle:"none"
+    }   
   };
   
   export default function Themestyling() {
     return (
-      <div style={person.theme}>
-        <h1>{person.name}'s Todos</h1>
-        <img style={person.theme1}
-          className="avatar"
-          src="me.jpg"
-          alt="Gregorio Y. Zara"
-        />
-        <ul>
-          <li>Improve the videophone</li>
-          <li>Prepare aeronautics lectures</li>
-          <li>Work on the alcohol-fuelled engine</li>
-        </ul>
+      <div style={person.theme1}>
+        <div style={person.imgStyle}>
+          <h1>{person.name}'s Todos</h1>
+        </div>
+
+        <div style={person.imgStyle}>
+          <img style={person.theme2}
+            src="me.jpg"
+            alt="Gregorio Y. Zara"
+          />
+        </div>
+        
+        <div style={person.imgStyle}>
+            <ul>
+              <li style={person.listStyle}>Improve the videophone</li>
+              <li style={person.listStyle}>Prepare aeronautics lectures</li>
+              <li style={person.listStyle}>Work on the alcohol-fuelled engine</li>
+            </ul>
+          </div>
       </div>
     );
   }
